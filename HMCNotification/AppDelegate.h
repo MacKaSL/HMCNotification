1afb2d0366925ab8c30ba3659b46a41d3c1bee90
MacKa <MacKa@MacKas-MacBook-Pro.local>
2017-07-24 11:59:33 +0530

    Adding the project

---
+++ HMCNotification/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HMCNotification
+//
+//  Created by Himal Madhushan on 7/24/17.
+//  Copyright © 2017 Himal Madhushan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HMCNotification/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  HMCNotification
+//
+//  Created by Himal Madhushan on 7/24/17.
+//  Copyright © 2017 Himal Madhushan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "HMCNotification.h"
+
+@interface ViewController : UIViewController
+
+
+@end
+
